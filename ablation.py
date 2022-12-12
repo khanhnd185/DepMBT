08bfd14ed05bd49a4c1cf90bd3a3052a1cda53f5
Khanh Nguyen <khanhnd185@gmail.com>
2022-12-12 17:00:08 +0900

    Ablation Study

---
+++ ablation.py
@@ -0,0 +1,209 @@
+from copy import deepcopy
+import os
+import torch
+import pandas
+import argparse
+from tqdm import tqdm
+from data import DVlog, collate_fn
+from sam import SAM
+from helpers import *
+from models import AblationModel
+from torch.utils.data import DataLoader
+
+
+def train(net, trainldr, optimizer, epoch, epochs, learning_rate, criteria):
+    total_losses = AverageMeter()
+    net.train()
+    train_loader_len = len(trainldr)
+    for batch_idx, data in enumerate(tqdm(trainldr)):
+        feature_audio, feature_video, mask, labels = data
+
+        # adjust_learning_rate(optimizer, epoch, epochs, learning_rate, batch_idx, train_loader_len)
+        feature_audio = feature_audio.cuda()
+        feature_video = feature_video.cuda()
+        mask = mask.cuda()
+        labels = labels.float()
+        labels = labels.cuda()
+        optimizer.zero_grad()
+
+        y = net(feature_audio, feature_video, mask)
+        loss = criteria(y, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_losses.update(loss.data.item(), feature_audio.size(0))
+    return total_losses.avg()
+
+def train_sam(net, trainldr, optimizer, epoch, epochs, learning_rate, criteria):
+    total_losses = AverageMeter()
+    net.train()
+    train_loader_len = len(trainldr)
+    for batch_idx, data in enumerate(tqdm(trainldr)):
+        feature_audio, feature_video, mask, labels = data
+
+        # adjust_learning_rate(optimizer, epoch, epochs, learning_rate, batch_idx, train_loader_len)
+        feature_audio = feature_audio.cuda()
+        feature_video = feature_video.cuda()
+        mask = mask.cuda()
+        labels = labels.float()
+        labels = labels.cuda()
+        optimizer.zero_grad()
+
+        y = net(feature_audio, feature_video, mask)
+        loss = criteria(y, labels)
+        loss.backward()
+        optimizer.first_step(zero_grad=True)
+
+        y = net(feature_audio, feature_video, mask)
+        criteria(y, labels).backward()
+        optimizer.second_step(zero_grad=True)
+
+        total_losses.update(loss.data.item(), feature_audio.size(0))
+    return total_losses.avg()
+
+def val(net, validldr, criteria):
+    total_losses = AverageMeter()
+    net.eval()
+    all_y = None
+    all_labels = None
+    for batch_idx, data in enumerate(tqdm(validldr)):
+        feature_audio, feature_video, mask, labels = data
+        with torch.no_grad():
+            feature_audio = feature_audio.cuda()
+            feature_video = feature_video.cuda()
+            mask = mask.cuda()
+            labels = labels.float()
+            labels = labels.cuda()
+
+            y = net(feature_audio, feature_video, mask)
+            loss = criteria(y, labels)
+            total_losses.update(loss.data.item(), feature_audio.size(0))
+
+            if all_y == None:
+                all_y = y.clone()
+                all_labels = labels.clone()
+            else:
+                all_y = torch.cat((all_y, y), 0)
+                all_labels = torch.cat((all_labels, labels), 0)
+    all_y = all_y >= 0.5
+    all_y = all_y.long().cpu().numpy()
+    all_labels = all_labels.cpu().numpy()
+    metrics = f1_score(all_labels, all_y)
+    return total_losses.avg(), metrics
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Train task seperately')
+
+    parser.add_argument('--config', '-c', type=int, default=7, help='Config number')
+    parser.add_argument('--resume', '-r', default='', help='Input file')
+    parser.add_argument('--batch', '-b', type=int, default=16, help='Batch size')
+    parser.add_argument('--rate', '-R', default='2', help='Batch size')
+    parser.add_argument('--epoch', '-e', type=int, default=10, help='Number of epoches')
+    parser.add_argument('--lr', '-a', type=float, default=0.00001, help='Learning rate')
+    parser.add_argument('--datadir', '-d', default='../../../Data/DVlog/', help='Data folder path')
+    parser.add_argument('--sam', '-s', action='store_true', help='Apply SAM optimizer')
+
+    args = parser.parse_args()
+    output_dir = 'ablation' + str(args.config) + '_' + args.rate 
+
+    trainset = DVlog(args.datadir+'train'+args.rate+'.pickle')
+    validset = DVlog(args.datadir+'valid'+args.rate+'.pickle')
+    train_criteria = nn.BCELoss()
+    valid_criteria = nn.BCELoss()
+
+    trainldr = DataLoader(trainset, batch_size=args.batch, collate_fn=collate_fn, shuffle=True, num_workers=0)
+    validldr = DataLoader(validset, batch_size=args.batch, collate_fn=collate_fn, shuffle=False, num_workers=0)
+
+    net = AblationModel(136, 25, 128, args.config)
+
+    if args.resume != '':
+        print("Resume form | {} ]".format(args.resume))
+        net = load_state_dict(net, args.resume)
+    net = nn.DataParallel(net).cuda()
+
+    if args.sam:
+        base_optimizer = torch.optim.SGD
+        optimizer = SAM(net.parameters(), base_optimizer, lr=args.lr, momentum=0.9, weight_decay=1.0/args.batch)
+    else:
+        optimizer = torch.optim.AdamW(net.parameters(), betas=(0.9, 0.999), lr=args.lr, weight_decay=1.0/args.batch)
+    best_performance = 0.0
+    epoch_from_last_improvement = 0
+
+    df = {}
+    df['epoch'] = []
+    df['lr'] = []
+    df['train_loss'] = []
+    df['val_loss'] = []
+    df['val_metrics'] = []
+
+    for epoch in range(args.epoch):
+        lr = optimizer.param_groups[0]['lr']
+        if args.sam:
+            train_loss = train_sam(net, trainldr, optimizer, epoch, args.epoch, args.lr, train_criteria)
+        else:
+            train_loss = train(net, trainldr, optimizer, epoch, args.epoch, args.lr, train_criteria)
+        val_loss, val_metrics = val(net, validldr, valid_criteria)
+
+        infostr = {'Downrate {}: {},{:.5f},{:.5f},{:.5f},{:.5f}'
+                .format(args.rate,
+                        epoch,
+                        lr,
+                        train_loss,
+                        val_loss,
+                        val_metrics)}
+        print(infostr)
+
+        os.makedirs(os.path.join('results', output_dir), exist_ok = True)
+
+        if val_metrics >= best_performance:
+            checkpoint = {
+                'epoch': epoch,
+                'val_loss': val_loss,
+                'val_metrics': val_metrics,
+                'state_dict': net.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }
+            torch.save(checkpoint, os.path.join('results', output_dir, 'best_val_perform.pth'))
+            best_performance = val_metrics
+            epoch_from_last_improvement = 0
+            best_model = deepcopy(net)
+        else:
+            epoch_from_last_improvement += 1
+
+        checkpoint = {
+            'epoch': epoch,
+            'state_dict': net.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }
+        torch.save(checkpoint, os.path.join('results', output_dir, 'cur_model.pth'))
+
+        df['epoch'].append(epoch)
+        df['lr'].append(lr)
+        df['train_loss'].append(train_loss)
+        df['val_loss'].append(val_loss)
+        df['val_metrics'].append(val_metrics)
+
+
+    validset = DVlog(args.datadir+'test'+args.rate+'.pickle')
+    valid_criteria = nn.BCELoss()
+    validldr = DataLoader(validset, batch_size=args.batch, collate_fn=collate_fn, shuffle=False, num_workers=0)
+
+    best_model = nn.DataParallel(best_model).cuda()
+    val_loss, val_metrics = val(best_model, validldr, valid_criteria)
+    print('Test set {}: {:.5f},{:.5f}'.format(args.rate, val_loss, val_metrics))
+
+    df['epoch'].append(args.epoch)
+    df['lr'].append(lr)
+    df['train_loss'].append(0)
+    df['val_loss'].append(val_loss)
+    df['val_metrics'].append(val_metrics)
+
+    df = pandas.DataFrame(df)
+    csv_name = os.path.join('results', output_dir, 'train.csv')
+    df.to_csv(csv_name)
+
+
+if __name__=="__main__":
+    main()
+
--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from timm.models.vision_transformer import DropPath, Mlp, Attention
 
 from layers import FusionBlock, get_projection
 from annotated_transformer import Encoder, Decoder
+from annotated_transformer import MultiHeadedAttention, PositionwiseFeedForward, clones, SublayerConnection
 
 class ShallowNN(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.Sigmoid, drop=0.):
@@ -80,7 +81,103 @@ class StanfordTransformerFusion(nn.Module):
         v = self.video_prj(v)
         a = self.audio_enc(a, m)
         v = self.video_enc(v, m)
-        f = self.fused_dec(a, v)
+        f = self.fused_dec(a, v, m)
+        f = f.mean(dim=1)
+        f = self.mlp(f).squeeze(-1)
+
+        return f
+
+
+
+'''
+    For Ablation Study
+'''
+class MaskedPassThrough(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, m=None):
+        return x
+
+class PassThroughAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, q, k, v, m=None):
+        return q
+
+class CustomDecoder(nn.Module):
+    def __init__(self, size, h, feed_forward, dropout):
+        super(CustomDecoder, self).__init__()
+        self.size = size
+        self.audio_attn = MultiHeadedAttention(h, size)
+        self.video_attn = MultiHeadedAttention(h, size)
+        self.fused_attn = MultiHeadedAttention(h, size*2)
+        self.feed_forward = PositionwiseFeedForward(size*2, feed_forward, dropout)
+        self.sublayer1 = clones(SublayerConnection(size, dropout), 2)
+        self.sublayer2 = clones(SublayerConnection(size*2, dropout), 2)
+
+    def forward(self, a, v, mask=None):
+        a = self.sublayer1[0](a, lambda a: self.audio_attn(a, v, v, mask))
+        v = self.sublayer1[1](v, lambda v: self.video_attn(v, a, a, mask))
+        f = torch.cat((a, v), dim=2)
+        f = self.sublayer2[0](f, lambda f: self.fused_attn(f, f, f, mask))
+        return self.sublayer2[1](f, self.feed_forward)
+
+class AblationModel(nn.Module):
+    def __init__(self, video_dimension, audio_dimension, fused_dimension, config_num):
+        super().__init__()
+        feed_forward = 256
+        dropout = 0.2
+        num_layers = 1
+        num_heads = 1
+
+        self.audio_prj = get_projection(audio_dimension, fused_dimension, 'minimal')
+        self.video_prj = get_projection(video_dimension, fused_dimension, 'minimal')
+
+        self_attention = config_num % 2
+        cross_attention = (config_num // 2) % 2
+        fused_attention = (config_num // 4) % 2
+
+        if self_attention:
+            self.audio_enc = Encoder(fused_dimension, num_heads, feed_forward, dropout, num_layers)
+            self.video_enc = Encoder(fused_dimension, num_heads, feed_forward, dropout, num_layers)
+        else:
+            self.audio_enc = MaskedPassThrough()
+            self.video_enc = MaskedPassThrough()
+
+        if cross_attention:
+            self.audio_attn = MultiHeadedAttention(num_heads, fused_dimension)
+            self.video_attn = MultiHeadedAttention(num_heads, fused_dimension)
+            self.sublayer1 = clones(SublayerConnection(fused_dimension, dropout), 2)
+        else:
+            self.audio_attn = PassThroughAttention()
+            self.video_attn = PassThroughAttention()
+            self.sublayer1 = [MaskedPassThrough() for _ in range(2)]
+
+        if fused_attention:
+            self.fused_attn = MultiHeadedAttention(num_heads, fused_dimension*2)
+            self.feed_forward = PositionwiseFeedForward(fused_dimension*2, feed_forward, dropout)
+            self.sublayer2 = clones(SublayerConnection(fused_dimension*2, dropout), 2)
+        else:
+            self.fused_attn = PassThroughAttention()
+            self.feed_forward = nn.Identity()
+            self.sublayer2 = [MaskedPassThrough() for _ in range(2)]
+
+        self.mlp = ShallowNN(fused_dimension*2, hidden_features=fused_dimension, out_features=1, drop=dropout)
+
+    def forward(self, a, v, m):
+        a = self.audio_prj(a)
+        v = self.video_prj(v)
+        a = self.audio_enc(a, m)
+        v = self.video_enc(v, m)
+
+        a = self.sublayer1[0](a, lambda a: self.audio_attn(a, v, v, m))
+        v = self.sublayer1[1](v, lambda v: self.video_attn(v, a, a, m))
+        f = torch.cat((a, v), dim=2)
+        f = self.sublayer2[0](f, lambda f: self.fused_attn(f, f, f, m))
+        f = self.sublayer2[1](f, self.feed_forward)
+
         f = f.mean(dim=1)
         f = self.mlp(f).squeeze(-1)
 
